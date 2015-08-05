11a0a7c43fc5da91e435aa47f5fc66884dee73fb
RikyReid <richardareid2002@googlemail.com>
2015-08-05 08:55:10 +0000

    Removed unwanted files.

--- curl_calls.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-HOST="127.0.0.1:3001"
-
-function post() {
-  curl -v -H "Content-Type: application/json" -XPOST \
-  --data "{\"username\":\"dickeyxxx\", \"body\":\"node rules!\"}" \
-  $HOST/api/posts
-}
